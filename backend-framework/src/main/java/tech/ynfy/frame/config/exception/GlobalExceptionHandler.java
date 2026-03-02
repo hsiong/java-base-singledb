edea18992d95d20441236849323dc6f8bc387739
hsiong <peak1203@126.com>
2026-03-02 17:07:30 +0800

    fix: close #15,close #16

--- backend-framework/src/main/java/tech/ynfy/frame/config/exception/GlobalExceptionHandler.java
+++ backend-framework/src/main/java/tech/ynfy/frame/config/exception/GlobalExceptionHandler.java
@@ -2,9 +2,12 @@ package tech.ynfy.frame.config.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import tech.ynfy.frame.module.Result;
+
+import java.util.stream.Collectors;
 
 /**
  * 全局异常处理器
@@ -14,6 +17,30 @@ import tech.ynfy.frame.module.Result;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+	
+	/**
+	 * spring-boot-starter-validation
+	 * 
+	 * @param ex
+	 * @return field validation error message
+	 */
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Result handleValidException(MethodArgumentNotValidException ex) {
+		String msg = ex.getBindingResult()
+					   .getFieldErrors()
+					   .stream()
+					   // key: msg
+					   .map(e -> e.getField() + ": " + e.getDefaultMessage())
+					   // 去重（可选）
+					   .distinct()
+					   // \n 分行
+					   .collect(Collectors.joining("\n"));
+		
+		if (msg == null || msg.isBlank()) {
+			msg = ex.getMessage();
+		}
+		return Result.error(msg);
+	}
 	
 	/**
 	 * 基础异常

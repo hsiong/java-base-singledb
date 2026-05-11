76ad3e8dbd160d1d574c6a7c21fb64f9e68c2ba0
hsiong <peak1203@126.com>
2026-05-11 16:35:54 +0800

    fix: close #21

--- backend-framework/src/main/java/tech/ynfy/frame/config/exception/BizException.java
+++ backend-framework/src/main/java/tech/ynfy/frame/config/exception/BizException.java
@@ -8,6 +8,12 @@ package tech.ynfy.frame.config.exception;
  * @since 11/19/25
  */
 public class BizException extends RuntimeException {
+	
+	/**
+	 * 默认业务异常错误码。
+	 */
+	public static final Integer BIZ_ERROR_CODE = 500;
+	
 	private final int code;
 	
 	public BizException(int code, String message) {
